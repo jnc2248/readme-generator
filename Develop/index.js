39426eb93671636d25790066f92381a64e2546d6
[j chun] <[justinchun224@gmail.com]>
2023-09-30 16:36:14 -0700

    created readme template literal and began writing inquirer prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,122 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [];
+const writeToFile = ({ title, description }) =>
+`
+# ${title}
+
+
+## Table of Contents
+
+1. [Description](#description)
+
+2. [Installation](#installation)
+
+3. [Usage](#usage)
+
+4. [License](#license)
+
+5. [Contributions](#contributions)
+
+6. [Tests] (#test)
+
+7. [Questions] (#questions)
+
+
+## Description
+
+${description}
+
+
+## Installation
+
+
+## Usage
+
+
+## License
+
+
+## Contributions
+
+
+## Tests
+
+
+## Questions
+
+
+`
+
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of this project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of the project.',
+        },
+        // {
+        //     type: 
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+        // {
+        //     type:
+        //     name:
+        //     message:
+        // },
+    ])
+    .then((answers) => {
+        const readmeTemplate = writeToFile(answers);
+
+        fs.writeFile('README.md', readmeTemplate, (err) =>
+            (err) ? console.error(err) : console.log("Successfully generated README!")
+        );
+    });
+
 
 // TODO: Create a function to initialize app
 function init() {}
